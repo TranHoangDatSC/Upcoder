9b980f6db29763d27e6fdfad94c29d0246d6f231
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-07 00:19:19 +0700

    Create gcdtest.cpp

---
+++ Study-Code/Upcoder/LTNC/De Quy/gcdtest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+int gcd(int a, int b)
+{
+    if(b == 0)
+        return a;
+    else return gcd(b,a%b);
+}
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << gcd(a,b);
+    return 0;
+}
