d9cb81ddf337e8cf07efc38a919405c7d2245eb7
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-06 23:12:33 +0700

    Update DEQUY4.cpp

---
+++ Study-Code/Upcoder/LTNC/De Quy/DEQUY4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int tong(int n, int x)
+{
+    if(n == 0)
+        return 1;
+    else 
+    {
+        return pow(x,n) + tong(n - 1, x);
+    }
+}
+
+int main()
+{
+    int n,x;
+    cin >> n >> x;
+    cout << tong(n,x);
+    return 0;
+}
