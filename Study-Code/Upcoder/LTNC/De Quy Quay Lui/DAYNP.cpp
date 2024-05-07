56d3ad13d19e7b83c7deac95e16030bb3feb0a5b
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-08 02:26:20 +0700

    Update and rename trxew.cpp to DAYNP.cpp

---
+++ Study-Code/Upcoder/LTNC/De Quy Quay Lui/DAYNP.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int n, arr[100];
+
+void print()
+{
+    for(int i = 1; i <= n; i++)
+    {
+        cout << arr[i];
+    }
+    cout << '\n';
+}
+
+void nhiPhan(int i)
+{
+    for(int j = 0; j <= 1; j++)
+    {
+        arr[i] = j;
+        if(i == n)
+            print();
+        else 
+            nhiPhan(i + 1);
+    }
+}
+
+int main()
+{
+    cin >> n;
+    nhiPhan(1);
+    return 0;
+}
