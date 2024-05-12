ea3852fbba1d2cef471e384dc14271bda32a83a1
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-12 21:42:54 +0700

    Create NGOACDUNG.cpp

---
+++ Study-Code/Upcoder/LTNC/Xu Ly Chuoi/NGOACDUNG.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    char x;
+    stack<char> st;
+    
+    while(cin >> x)
+    {
+        if(st.empty())
+            st.push(x);
+            
+        else
+        {
+            if((st.top() == '(' && x == ')') ||
+            (st.top() == '{' && x == '}') ||
+            st.top() == '[' && x == ']')
+                st.pop();
+            else
+                st.push(x);
+        }
+    }
+    cout << (st.empty() ? "yes" : "no");
+    return 0;
+}
