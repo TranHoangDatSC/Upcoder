00b3423bd19d733a604e5a1fb70bc3d075c1a17c
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-12 16:01:25 +0700

    Create KYTUKHONGLAP(28tech).cpp

---
+++ Study-Code/Upcoder/LTNC/Xu Ly Chuoi/KYTUKHONGLAP(28tech).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+    
+    while(n--)
+    {
+        string str;
+        getline(cin,str);
+        
+        map<char, int> mp;
+        for(char x : str)
+        {
+            mp[x]++;
+        }
+        for(char x : str)
+        {
+            if(mp[x] == 1)
+                cout << x;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
