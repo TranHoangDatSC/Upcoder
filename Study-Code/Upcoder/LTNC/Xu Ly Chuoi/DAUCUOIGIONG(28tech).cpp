00ae556aa2ff87710399fe1341ad4ace365253eb
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-12 15:40:07 +0700

    Update DAUCUOIGIONG(28tech).cpp

--- Study-Code/Upcoder/LTNC/Xu Ly Chuoi/DAUCUOIGIONG(28tech).cpp
+++ Study-Code/Upcoder/LTNC/Xu Ly Chuoi/DAUCUOIGIONG(28tech).cpp
@@ -34,3 +34,40 @@ int main()
 // Output:
 // 12 
 // Vi chuoi co 7 xau con,  b-b-b thi co them 3 xau, hai cap a-a c-c la 2, 7+3+2 == 12
+
+/*
+Other way:
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+    
+    while(n--)
+    {
+        string str;
+        getline(cin,str);
+        
+      //map<char, int> mp;
+        int arr[256] = {0}; // ASCII
+        for(char x : str)
+        {
+            arr[x]++;
+        }
+        long long ams = str.size(); // cac xau con co mot ky tu8
+        for(int i = 0; i < 256; i++)
+        {
+            if(arr[i] != 0)
+                ams += arr[i] * (arr[i] - 1) / 2;
+        }
+        cout << ams << "\n";
+    }
+    return 0;
+}
+*/
