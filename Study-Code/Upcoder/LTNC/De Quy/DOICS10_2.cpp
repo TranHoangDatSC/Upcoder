d2d60e0f0da5341607fc59b08a4fdda0971a5d61
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-14 14:00:49 +0700

    Create DOICS10_2.cpp

---
+++ Study-Code/Upcoder/LTNC/De Quy/DOICS10_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void coSo2(int n)
+{
+    if(n < 2)
+        cout << n;
+    else
+    {
+        coSo2(n / 2);
+        cout << n % 2;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    coSo2(n);
+    return 0;
+}
