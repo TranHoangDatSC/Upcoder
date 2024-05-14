24d99eab177417bc6dbd27be5daddbfdc3e1683c
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-14 15:53:13 +0700

    Update DEQUY9.cpp

--- Study-Code/Upcoder/LTNC/De Quy/DEQUY9.cpp
+++ Study-Code/Upcoder/LTNC/De Quy/DEQUY9.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 using namespace std;
 
+long Sum(long n)
+{
+    if(n == 1)  return 1;
+    return n + Sum(n - 1);
+}
+long S(long n)
+{
+    long res = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        res += Sum(i);
+    }
+    return res;
+}
+long factorial(long n)
+{
+    if(n == 1) return 1;
+    return n * factorial(n - 1);
+}
+long P(long n)
+{
+    long res = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        res += factorial(i);
+    }
+    return res;
+}
+
+int main()
+{
+    long n; 
+    cin >> n;
+    if(n <= 20)
+    {
+        cout << "S(" << n << ") = " << S(n) << "\n";
+        cout << "P(" << n << ") = " << P(n);
+    } 
+    return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
 long tongS(long n)
 {
     if(n == 0)
@@ -27,3 +72,4 @@ int main()
     cout << "\nP(" << n << " ) = " << tongP(n);
     return 0;
 }
+*/
