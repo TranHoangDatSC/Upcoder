65a8a247aa2eb84c3a7f7298994b8bd3c85db784
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-15 10:23:56 +0700

    Create LTNC_20_HK2_Cau2.3.cpp

---
+++ Study-Code/Upcoder/LTNC/De Quy/LTNC_20_HK2_Cau2.3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double tong(int n)
+{
+    if(n == 1) return sqrt(2);
+    return sqrt(2 + tong(n - 1));
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << roundf(tong(n) * 1000) / 1000;
+    return 0;
+}
