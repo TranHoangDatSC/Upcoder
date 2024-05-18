ae6c5e938957a67256fafc57d8194144691d7ec3
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-18 20:56:45 +0700

    Create DAYGIAITHUA.cpp

---
+++ Study-Code/Upcoder/LTNC/De Quy/DAYGIAITHUA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+long FAC(int n);
+
+int main(){
+    int n;
+    cin >> n;
+    cout << FAC(n);
+    return 0;
+}
+
+long FAC(int n)
+{
+    if(n < 2) return 1;
+    else return n * FAC(n - 1);
+}
