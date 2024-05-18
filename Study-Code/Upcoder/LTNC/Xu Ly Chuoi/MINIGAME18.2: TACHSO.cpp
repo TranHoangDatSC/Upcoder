447ac374b343faa55de4ad5b7419e284199d3256
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-18 19:39:50 +0700

    Update MINIGAME18.2: TACHSO.cpp

--- Study-Code/Upcoder/LTNC/Xu Ly Chuoi/MINIGAME18.2: TACHSO.cpp
+++ Study-Code/Upcoder/LTNC/Xu Ly Chuoi/MINIGAME18.2: TACHSO.cpp
@@ -25,3 +25,23 @@ int main()
     cout << sum; 
     return 0;
 }
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    int cnt = 0;
+    
+    while(getline(cin,str,','))
+    {
+        cout << str << "\n";
+        cnt += stoi(str);
+    }
+    cout << cnt;
+    return 0;
+}
+*/
