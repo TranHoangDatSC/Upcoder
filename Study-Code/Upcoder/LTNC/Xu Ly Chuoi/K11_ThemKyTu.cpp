fb34750036b25c3fe1fd8241b9d28c781e8ce175
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-18 19:19:29 +0700

    Create K11_ThemKyTu.cpp

---
+++ Study-Code/Upcoder/LTNC/Xu Ly Chuoi/K11_ThemKyTu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str, s;
+    
+    getline(cin, str);
+    cin >> s;
+    
+    cout << str << s;
+    return 0;
+}
