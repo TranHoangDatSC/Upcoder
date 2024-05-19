f13060f8e25047c20e9580cf2f8948cb8e8a0332
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-19 08:42:00 +0700

    Update TAMGIACSO.cpp

--- Study-Code/Upcoder/LTNC/De Quy Quay Lui/TAMGIACSO.cpp
+++ Study-Code/Upcoder/LTNC/De Quy Quay Lui/TAMGIACSO.cpp
@@ -73,3 +73,81 @@ int main()
     cout << "\n" << maxSum;
     return 0;
 }
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+
+int sum;
+int arr[100][100];
+int maxSum = 0;
+
+int dx[] = {1,1};
+int dy[] = {0,1};
+
+vector<int> save, way;
+
+void Try(int x, int y)
+{
+    if(x == n)
+    {
+        if(sum > maxSum)
+        {
+            maxSum = sum;
+            save = way;
+        }
+    }
+    
+    else 
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            
+            if(1 <= xx && xx <= n && 1 <= yy & yy <= xx)
+            {
+                sum += arr[xx][yy];
+                way.push_back(arr[xx][yy]);
+                
+                Try(xx,yy);
+                
+                sum -= arr[xx][yy];
+                way.pop_back();
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio;
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    
+    sum = arr[1][1];
+    way.push_back(arr[1][1]);
+    
+    Try(1,1);
+    
+    for(int x : save)
+    {
+        cout << x << " ";
+    }
+    cout << "\n" << maxSum; 
+    
+    return 0;
+}
+*/
