fbca7853571cedfddd19498645f99584feb25df8
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-30 00:45:43 +0700

    Create Thang_co_may_ngay.cpp

---
+++ Study-Code/28tech_C++/Re_nhanh/Thang_co_may_ngay.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    switch(n)
+    {
+        case 1 : case 3 : case 5 : case 7 : case 8 : case 10 : case 12 :
+            cout << "Thang " << n << " co 31 ngay";
+            break;
+            
+        case 4 : case 6 : case 9 : case 11 :
+            cout << "Thang " << n << " co 30 ngay";
+            break;
+        case 2 :
+            cout << "Thang " << n << " co 28 hoac 29 ngay";
+            break;
+            
+        default : cout << "Khong hop le";
+    }
+    return 0;
+}
