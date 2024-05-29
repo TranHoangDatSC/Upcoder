3102545cae2dbcb6b7a62c7774ed2c964e0be6a8
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-29 20:50:31 +0700

    Update MAXIUMSTRING.cpp

--- Study-Code/Upcoder/LTNC/Xu Ly Chuoi/MAXIUMSTRING.cpp
+++ Study-Code/Upcoder/LTNC/Xu Ly Chuoi/MAXIUMSTRING.cpp
@@ -1,3 +1,75 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+    int m, s; 
+    cin >> m >> s;
+
+    if ((m > 1 && s == 0) || s > m * 9)
+    {
+        cout << -1;
+        return 0;
+    }
+    else if (m == 1 && s == 0)
+    {
+        cout << 0 << endl << 0;
+        return 0;
+    }
+    
+    int s2 = s;
+    int min[m], max[m];
+    
+    memset(min,0,sizeof(min));
+    memset(max,0,sizeof(max));
+    
+    for(int i = m - 1; i >= 0; i--)
+    {
+        if(s >= 9)
+        {
+            min[i] += 9;
+            s -= 9;
+        }
+        else if(s > 0)
+        {
+            min[i] += s;
+            s = 0;
+        }
+    }
+    
+    if(min[0] == 0)
+    {
+        int i = 1;
+        while(min[i] == 0)
+            i++;
+            
+        min[0] += 1; min[i] -= 1;
+    }
+    for(int i = 0; i < m; i++)
+    {
+        if(s2 >= 9)
+        {
+            max[i] += 9;
+            s2 -= 9;
+        }
+        else if(s2 > 0)
+        {
+            max[i] += s2;
+            s2 = 0;
+        }
+    }
+    
+    for(int z : min)
+        cout << z;
+    
+    cout << endl;
+    
+    for(int z : max)
+        cout << z;
+    
+    return 0;
+}
+
+
 /*
 #include<iostream>
 #include<cstring>
