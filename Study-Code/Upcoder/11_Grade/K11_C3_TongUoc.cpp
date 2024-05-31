6900b7f8748eefd63a3c8b18d6af183e985eb05d
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-05-31 20:42:41 +0700

    Update K11_C3_TongUoc.cpp

--- Study-Code/Upcoder/11_Grade/K11_C3_TongUoc.cpp
+++ Study-Code/Upcoder/11_Grade/K11_C3_TongUoc.cpp
@@ -5,6 +5,17 @@ int main()
 {
     int n, s = 0;
     cin >> n;
+    
+    if(n == 0)
+    {
+        cout << 0;
+        return 0;
+    }
+    if(n < 0)
+    {
+        n = -n;
+    }
+    
     for(int i = 1; i <= n; i++)
     {
         if(n % i == 0)
