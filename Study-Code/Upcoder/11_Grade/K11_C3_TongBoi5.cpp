aa1319ad99d62773a327d6793d6b2e3fc44232c9
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 05:58:10 +0700

    Create K11_C3_TongBoi5.cpp

---
+++ Study-Code/Upcoder/11_Grade/K11_C3_TongBoi5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int sum = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        if(i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+    cout << sum;
+    
+    return 0;
+}
