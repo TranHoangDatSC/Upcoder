75ba436d55aaacbbdce640c1f8ee35f7994296d4
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 15:46:57 +0700

    Create Chan_le.cpp

---
+++ Study-Code/28tech_C++/Re_nhanh/Chan_le.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    
+    cout << (a % 2 == 0 ? 0 : 1); 
+    return 0;
+}
