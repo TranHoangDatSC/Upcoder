df42197991128514f41455d0845e5f6dd90b1e81
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 15:40:05 +0700

    Create Tinh_tong6.cpp

---
+++ Study-Code/28tech_C++/Re_nhanh/Tinh_tong6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    long long a,b;
+    cin >> a >> b ;
+    
+    cout << a / b * b;
+    return 0;
+}
