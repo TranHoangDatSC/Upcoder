cd06319c66e3d53a930d1d988e11602b6da949d0
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 16:15:04 +0700

    Update Be_or_Lon_Nhat.cpp

--- Study-Code/28tech_C++/Re_nhanh/Be_or_Lon_Nhat.cpp
+++ Study-Code/28tech_C++/Re_nhanh/Be_or_Lon_Nhat.cpp
@@ -21,3 +21,35 @@ int main()
     
     return 0;
 }
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    
+    int max, min;
+    
+    if(a > b && a > c)
+        max = a;
+    else if(b > a && b > c)
+        max = b;
+    else if(c > a && c > b)
+        max = c;
+        
+    if(a < b && a < c)
+        min = a;
+    else if(b < a && b < c)
+        min = b;
+    else if(c < a && c < b)
+        min = c;
+        
+    cout << max << " " << min;
+    
+    return 0;
+}
+*/
