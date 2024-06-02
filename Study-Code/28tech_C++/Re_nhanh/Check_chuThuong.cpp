6c3917b51401b8da9d82eab30ffb115986b217e4
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 16:02:06 +0700

    Update Check_chuThuong.cpp

--- Study-Code/28tech_C++/Re_nhanh/Check_chuThuong.cpp
+++ Study-Code/28tech_C++/Re_nhanh/Check_chuThuong.cpp
@@ -13,3 +13,20 @@ int main()
         cout << "NO";
     return 0;
 }
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    
+    if(a >= 'a' && a <= 'z')
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
+*/
