4a83dad466dfb9e028734a24cbabee6d5c5b9711
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 14:55:29 +0700

    Create Tinh_tong1.cpp

---
+++ Study-Code/28tech_C++/Re_nhanh/Tinh_tong1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int sum(int n)
+{
+    if(n == 1) return 1;
+    else return n + sum(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    cout << sum(n);
+    return 0;
+}
