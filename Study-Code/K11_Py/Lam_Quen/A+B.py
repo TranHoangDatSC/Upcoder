39cd283fd2498e0b0657ffe63f0bf6d3705e109d
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 14:00:25 +0700

    Create A+B.py

---
+++ Study-Code/K11_Py/Lam_Quen/A+B.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+
+print(a + b)
+
+"""
+def sum(a, b):
+  return (a + b)
+
+a = int(input('Enter A: '))
+b = int(input('Enter B: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+"""
