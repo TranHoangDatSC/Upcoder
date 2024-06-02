8ffcb9d41f1d8a037fa1e44803844148409a5e48
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 14:53:56 +0700

    Create Sqrt().cpp

---
+++ Study-Code/28tech_C++/Re_nhanh/Sqrt().cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    double a = sqrt(n);
+    
+    cout << a;
+    
+    return 0;
+}
