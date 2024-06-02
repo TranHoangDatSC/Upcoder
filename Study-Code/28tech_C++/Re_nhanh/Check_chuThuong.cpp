d428dcf6369e71be15aacb840d1d5707c272f3c7
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-02 16:01:30 +0700

    Create Check_chuThuong.cpp

---
+++ Study-Code/28tech_C++/Re_nhanh/Check_chuThuong.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    
+    if(a >= 97 && a <= 122)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
