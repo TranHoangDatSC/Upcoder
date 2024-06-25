6acce24a915422f165dde08bf650068df2008234
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-25 17:05:34 +0700

    Update DFS.cpp

--- Study-Code/Upcoder/Graph_Theory/Depth_First_Search/DFS.cpp
+++ Study-Code/Upcoder/Graph_Theory/Depth_First_Search/DFS.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int nV,nE,start; // nV = số đỉnh, nE = số cạnh;
+int nV, nE, start; // Số đỉnh, số cạnh
 vector<int> adj[100]; // Danh sách kề
-bool visited[100];
+bool visited[100]; // Đánh dấu
 
-void DFS(int u)
-{
-    cout << u << " ";
-    visited[u] = true;
-    for(int v : adj[u])
-    {
-        if(!visited[v])
-            DFS(v);
-    }
-}
+void DFS(int u); // Hàm DFS
 
 int main()
 {
@@ -26,12 +17,39 @@ int main()
         cin >> x >> y;
         
         adj[x].push_back(y);
-        adj[y].push_back(x); // Nếu là đồ thị có hướng thì bỏ
-
+        adj[y].push_back(x); // Vector có hướng thì bỏ
     }
     
-    memset(visited,false,sizeof(visited)); // Đảm bảo toàn bộ visited = false;
+    memset(visited,false,sizeof(visited)); // Đảm bảo toàn bộ visited đều false
     DFS(start);
     
     return 0;
 }
+
+void DFS(int u)
+{
+    cout << u << " ";
+    visited[u] = true;
+    
+    for(int v : adj[u])
+    {
+        if(!visited[v])
+        {
+            DFS(v);
+        }
+    }
+}
+
+/* Nếu là đồ thị có hướng:
+Input:
+6 6 1
+1 2
+1 3
+1 5
+2 4
+3 5
+6 5
+
+Output:
+1 2 4 3 5
+*/
