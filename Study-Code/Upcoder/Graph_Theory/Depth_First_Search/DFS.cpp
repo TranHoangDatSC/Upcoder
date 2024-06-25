c26bfe5e43f3d0e625ac8a2bc816ab93aa08ae2b
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-25 16:11:48 +0700

    Create DFS.cpp

---
+++ Study-Code/Upcoder/Graph_Theory/Depth_First_Search/DFS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nV,nE,start; // nV = số đỉnh, nE = số cạnh;
+vector<int> adj[100]; // Danh sách kề
+bool visited[100];
+
+void DFS(int u)
+{
+    cout << u << " ";
+    visited[u] = true;
+    for(int v : adj[u])
+    {
+        if(!visited[v])
+            DFS(v);
+    }
+}
+
+int main()
+{
+    cin >> nV >> nE >> start;
+    
+    for(int i = 0; i < nE; i++)
+    {
+        int x,y;
+        cin >> x >> y;
+        
+        adj[x].push_back(y);
+        adj[y].push_back(x); // Nếu là đồ thị có hướng thì bỏ
+
+    }
+    
+    memset(visited,false,sizeof(visited)); // Đảm bảo toàn bộ visited = false;
+    DFS(start);
+    
+    return 0;
+}
