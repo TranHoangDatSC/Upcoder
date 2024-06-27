856058b1cf6b8f519b3b1296dbe5c1db243984db
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-06-28 06:30:15 +0700

    Create BFS.cpp

---
+++ Study-Code/Upcoder/Graph_Theory/Breadth_First_Search/BFS.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nV,nE,start; // So dinh, so canh
+vector<int> adj[100]; // Luu danh sach ke
+bool visited[100];
+
+void BFS(int u);
+
+int main()
+{
+    cin >> nV >> nE >> start;
+    
+    for(int i = 0; i < nE; i++)
+    {
+        int x,y;
+        cin >> x >> y;
+        
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    memset(visited,false,sizeof(visited));
+
+    BFS(start);
+    
+    return 0;
+}
+
+void BFS(int u)
+{
+    queue<int> q;
+    q.push(u);
+    visited[u] = true;
+    
+    while(!q.empty())
+    {
+        int v = q.front(); // Lay dinh dau hang doi
+        q.pop();
+        cout << v << " ";
+        
+        for(int x : adj[v])
+        {
+            if(!visited[x])
+            {
+                q.push(x);
+                visited[x] = true;
+            }
+        }
+
+    }
+}
