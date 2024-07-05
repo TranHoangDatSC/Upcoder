ba92c1db2bb3600bef8eff0b257937a448d30dab
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-05 10:19:19 +0700

    Create Interchange_sort & swap().cpp

---
+++ Study-Code/Some_Function_C++/Sort/Interchange_sort & swap().cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void swap_num(int &a, int &b)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void Interchange_sort(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+        for(int j = i + 1; j < size; j++)
+            if(arr[i] > arr[j])
+                swap_num(arr[i],arr[j]);
+}
+
+int main()
+{
+    int arr[100], size = 0;
+    while(cin >> arr[size])
+        size++;
+        
+    selection_sort(arr,size);
+    
+    for(int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+        
+    return 0;
+}
