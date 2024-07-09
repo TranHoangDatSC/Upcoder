f37cab2f1067843aec22b8b9d4363f53f200d588
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-09 16:48:19 +0700

    Update DSLK_NhapXuat.cpp

--- Study-Code/Upcoder/LTNC/Pointer/DSLK_NhapXuat.cpp
+++ Study-Code/Upcoder/LTNC/Pointer/DSLK_NhapXuat.cpp
@@ -1,63 +1,68 @@
 #include <iostream>
 using namespace std;
 
-struct Node
-{
-    int data;
+struct Node {
     Node *next;
+    int data;
     
-    Node(int value)
+    Node(int value) 
     {
         data = value;
         next = nullptr;
     }
 };
 
-struct LinkedList
-{
+struct LinkedList {
     Node *head;
-
+    
     LinkedList() : head(nullptr) {}
     
-    void push(int n)
+    void add(int n) 
     {
         Node *node = new Node(n);
         
-        if(head == nullptr)
+        if(head == nullptr) 
+        {
             head = node;
-        else
+        }
+        
+        else 
         {
             Node *tmp = head;
-            
-            while(tmp->next != nullptr)
+            while(tmp->next != nullptr) 
+            {
                 tmp = tmp->next;
+            }
             tmp->next = node;
         }
     }
     
-    void print()
+    void print() 
     {
         Node *tmp = head;
+        
         while(tmp != nullptr)
         {
             cout << tmp->data << " ";
             tmp = tmp->next;
         }
     }
-    
     ~LinkedList() {}
-};
+};  
 
-int main()
+int main() 
 {
     LinkedList list;
-    int tmp;
+    int n, tmp; 
+    cin >> n;
     
-    while(cin >> tmp && tmp != -1)
+    for(int i = 0; i < n; i++)
     {
-        list.push(tmp);
+        cin >> tmp;
+        list.add(tmp);
     }
-    cout << "DSLK ban dau: ";
+    
     list.print();
+    
     return 0;
 }
