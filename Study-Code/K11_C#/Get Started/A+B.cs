2b28eb971ec1bb81ffd5f35f4510a88379251e3c
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-13 16:40:09 +0700

    Create A+B.cs

---
+++ Study-Code/K11_C#/Get Started/A+B.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{ 
+    public class Program {
+        public static void Main(string[] args) {
+            
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(A + B);
+        }
+    }
+}
