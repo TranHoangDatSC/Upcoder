2ff8ebcb5972b680df223d7fa1b7c2f0ec34121c
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-13 20:44:52 +0700

    Update TKN_K11_Sin.cs

--- Study-Code/K11_C#/Get Started/TKN_K11_Sin.cs
+++ Study-Code/K11_C#/Get Started/TKN_K11_Sin.cs
@@ -6,8 +6,8 @@ namespace Practice_C_Sharp
     {
         public static void Main(string[] args)
         {
-            double a = Convert.ToInt32(Console.ReadLine());
-            double b = Convert.ToInt32(Console.ReadLine());
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
 
             double result = (a+Math.Sin(b)) / (Math.Sqrt(a*a + b*b + 1));
 
