7d3fb3be6f504378903ceced6331daf4a6b90b0f
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-13 20:44:03 +0700

    Update K11_HinhVuong.cs

--- Study-Code/K11_C#/Get Started/K11_HinhVuong.cs
+++ Study-Code/K11_C#/Get Started/K11_HinhVuong.cs
@@ -6,7 +6,7 @@ namespace Practice_C_Sharp
     {
         public static void Main(string[] args)
         {
-            double a = Convert.ToInt32(Console.ReadLine());
+            double a = Convert.ToDouble(Console.ReadLine());
 
             if (a < 0) { Console.Write("NHAP SAI"); }
 
