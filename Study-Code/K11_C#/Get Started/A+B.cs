d52f6deb6b6574acc086bdbae38aed21f2ce6ca2
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-13 20:09:00 +0700

    Update A+B.cs

--- Study-Code/K11_C#/Get Started/A+B.cs
+++ Study-Code/K11_C#/Get Started/A+B.cs
@@ -1,13 +1,14 @@
 using System;
 
-namespace MyApplication
-{ 
-    public class Program {
-        public static void Main(string[] args) {
-            
+namespace Practice_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
             int A = Convert.ToInt32(Console.ReadLine());
             int B = Convert.ToInt32(Console.ReadLine());
-            
+
             Console.WriteLine(A + B);
         }
     }
