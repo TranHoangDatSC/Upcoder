fe55a1597feaf6575a1b3995ce9347208ffe86a8
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-13 20:44:34 +0700

    Update TKN_K11_Cos.cs

--- Study-Code/K11_C#/Get Started/TKN_K11_Cos.cs
+++ Study-Code/K11_C#/Get Started/TKN_K11_Cos.cs
@@ -6,7 +6,7 @@ namespace Practice_C_Sharp
     {
         public static void Main(string[] args)
         {
-            double a = Convert.ToInt32(Console.ReadLine());
+            double a = Convert.ToDouble(Console.ReadLine());
 
             double result = (Math.Abs(2*a + 3) / Math.Cos(a)) + Math.Sqrt(2 + Math.Cos(a));
 
