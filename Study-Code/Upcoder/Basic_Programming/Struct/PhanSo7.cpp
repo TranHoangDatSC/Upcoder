3d39eda10d7bc61201262efeb855ea09eb5d49f1
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-07-28 15:01:48 +0700

    Update PhanSo7.cpp

--- Study-Code/Upcoder/Basic_Programming/Struct/PhanSo7.cpp
+++ Study-Code/Upcoder/Basic_Programming/Struct/PhanSo7.cpp
@@ -1,4 +1,92 @@
 #include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+struct PhanSo {
+    int tu, mau;
+
+    friend istream& operator >> (istream& is, PhanSo& ps) {
+        is >> ps.tu >> ps.mau;
+        return is;
+    }
+
+    friend ostream& operator << (ostream& os, PhanSo ps) {
+        if(ps.tu == 0) os << 0;
+        
+        else os << ps.tu << "/" << ps.mau;
+        
+        return os;
+    }
+
+    void RutGon() {
+        int d = gcd(this->tu, this->mau);
+        this->tu /= d;
+        this->mau /= d;
+        
+        if(this->mau < 0) {
+            this->tu = -this->tu;
+            this->mau = -this->mau;
+        }
+    }
+
+    PhanSo operator + (PhanSo other) {
+        PhanSo kq;
+        kq.tu = this->tu * other.mau + this->mau * other.tu;
+        kq.mau = this->mau * other.mau;
+        kq.RutGon();
+        return kq;
+    }
+
+    PhanSo operator - (PhanSo other) {
+        PhanSo kq;
+        kq.tu = this->tu * other.mau - this->mau * other.tu;
+        kq.mau = this->mau * other.mau;
+        kq.RutGon();
+        return kq;
+    }
+
+    PhanSo operator * (PhanSo other) {
+        PhanSo kq;
+        kq.tu = this->tu * other.tu;
+        kq.mau = this->mau * other.mau;
+        kq.RutGon();
+        return kq;
+    }
+
+    PhanSo operator / (PhanSo other) {
+        PhanSo kq;
+        kq.tu = this->tu * other.mau;
+        kq.mau = this->mau * other.tu;
+        kq.RutGon();
+        return kq;
+    }
+};
+
+int main() {
+    PhanSo a, b;
+    cin >> a >> b;
+
+    if (a.mau == 0 || b.mau == 0) {
+        cout << -1;
+        return 0;
+    }
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    if(b.tu != 0) cout << a / b;
+
+    return 0;
+}
+
+
+/*
+
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
@@ -98,3 +186,4 @@ int main() {
 
     return 0;
 }
+*/
