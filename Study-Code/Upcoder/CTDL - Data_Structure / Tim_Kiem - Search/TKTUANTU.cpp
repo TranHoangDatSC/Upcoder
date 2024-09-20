537aef16b161762469cec17a60802a43c6d69407
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-09-20 13:30:54 +0700

    Update TKTUANTU.cpp

--- Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTUANTU.cpp
+++ Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTUANTU.cpp
@@ -28,3 +28,32 @@ int main()
     
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+int linear_search(int arr[],int n,int x) {
+    int res = -1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    int n,x;
+    int arr[100];
+    
+    cin >> n >> x;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    cout << linear_search(arr,n,x);
+
+    return 0;
+}
+*/
