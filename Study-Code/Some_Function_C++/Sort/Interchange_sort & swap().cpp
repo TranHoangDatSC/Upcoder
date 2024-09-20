db9a0a8132eacbe07c7516b47ec3f0b6d8130f98
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-09-20 14:35:22 +0700

    Update Interchange_sort & swap().cpp

--- Study-Code/Some_Function_C++/Sort/Interchange_sort & swap().cpp
+++ Study-Code/Some_Function_C++/Sort/Interchange_sort & swap().cpp
@@ -1,37 +1,56 @@
 #include <iostream>
 using namespace std;
 
-void swap_num(int &a, int &b)
-{
-    int tmp = a;
+struct Diem {
+    int diem;  
+    
+    friend istream& operator >> (istream& is, Diem& d) {
+        is >> d.diem;
+        return is;
+    }
+    
+    friend ostream& operator << (ostream& os, Diem d) {
+        cout << d.diem;
+        return os;
+    }
+    
+    bool operator < (Diem other) {
+        return this->diem < other.diem;
+    }
+};
+
+void swap(Diem &a,Diem &b) {
+    Diem tmp = a;
     a = b;
     b = tmp;
 }
 
-void Interchange_sort(int arr[], int size)
-{
-    for(int i = 0; i < size; i++)
-        for(int j = i + 1; j < size; j++)
-            if(arr[i] > arr[j])
-                swap_num(arr[i],arr[j]);
+template <class Diem>
+void interchange_sort(Diem a[],int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(a[j] < a[i]) 
+                swap(a[j],a[i]);
+        }
+    }
 }
 
-int main()
-{
-    int arr[100], size = 0;
-    while(cin >> arr[size])
-        size++;
-        
-    Interchange_sort(arr,size);
+int main() {
+    Diem a[100];
+    int n = 0;
     
-    for(int i = 0; i < size; i++)
-        cout << arr[i] << " ";
+    while(cin >> a[n]) n++;
+    
+    interchange_sort(a,n);
+    
+    for(int i = 0; i < n; i ++) cout << a[i] << " ";
     
     return 0;
 }
+/*
+input
+2 1 3 0
 
-/* 
-Input: 3 4 5 2 1
-Output: 1 2 3 4 5
-
+output
+0 1 2 3
 */
