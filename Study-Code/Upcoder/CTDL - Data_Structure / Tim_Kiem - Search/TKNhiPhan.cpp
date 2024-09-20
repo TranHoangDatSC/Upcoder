efc38426d19291034e06a9f59929ac7bcf45d209
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-09-20 14:04:20 +0700

    Update TKNhiPhan.cpp

--- Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKNhiPhan.cpp
+++ Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKNhiPhan.cpp
@@ -44,3 +44,53 @@ int main()
     
     return 0;
 }
+/*
+#include <iostream>
+using namespace std;
+
+int binary_search_recursive(int arr[], int lef, int rig, int x)
+{
+    if (lef <= rig)
+    {
+        int m = lef + (rig - lef) / 2; // Avoid potential overflow
+        
+        // Check if x is present at mid
+        if (arr[m] == x)
+        {
+            return m;
+        }
+        // If x is greater, search in the right half
+        else if (arr[m] < x)
+        {
+            return binary_search_recursive(arr, m + 1, rig, x);
+        }
+        // If x is smaller, search in the left half
+        else
+        {
+            return binary_search_recursive(arr, lef, m - 1, x);
+        }
+    }
+    
+    return -1; // Element not found
+}
+
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    
+    int arr[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    // Call the recursive binary search function
+    int res = binary_search_recursive(arr, 0, n - 1, x);
+    cout << res;
+    
+    return 0;
+}
+
+*/
