f7b0146095b905fa375b6f7b7919726f6851c3a5
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-10-02 01:16:48 +0700

    Update Lab_4_Kruskal().cpp

--- Study-Code/Upcoder/Graph_Theory/Lab_Excerise/Lab_4_Kruskal().cpp
+++ Study-Code/Upcoder/Graph_Theory/Lab_Excerise/Lab_4_Kruskal().cpp
@@ -146,3 +146,157 @@ int main() {
     printSpanningTree(g, "output.txt");
     return 0;
 }
+
+/*
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Edge {
+    int u, v, w;
+};
+
+struct Graph {
+    int vertices;
+    vector<Edge> T;
+    vector<vector<int>> adj;
+    vector<vector<int>> saveMatrix;
+
+    Graph() : vertices(0), adj() {}
+};
+
+void readGraph(Graph& g, string fn) {
+    ifstream f(fn);
+    if (f.is_open()) {
+        f >> g.vertices;
+        g.adj.resize(g.vertices, vector<int>(g.vertices, 0));
+        g.saveMatrix.resize(g.vertices, vector<int>(g.vertices, 0));
+        for (int i = 0; i < g.vertices; i++) {
+            for (int j = 0; j < g.vertices; j++) {
+                f >> g.adj[i][j];
+                g.saveMatrix[i][j] = g.adj[i][j];
+            }
+        }
+        f.close();
+    }
+    else {
+        cout << "Khong mo duoc file input!" << endl;
+    }
+}
+
+void Init_Arr_Edge(int& nEdge, vector<Edge>& listE, Graph g) {
+    listE.clear();
+    for (int i = 0; i < g.vertices; i++) {
+        for (int j = i + 1; j < g.vertices; j++) {
+            if (g.adj[i][j] != 0) {
+                listE.push_back({ i, j, g.adj[i][j] });
+            }
+        }
+    }
+    nEdge = listE.size();
+}
+
+void Sort_Edge(vector<Edge>& e, int nE) {
+    sort(e.begin(), e.end(), [](const Edge& a, const Edge& b) {
+        return a.u < b.u;
+        });
+}
+
+bool DFS(int v, vector<vector<int>>& tree, vector<bool>& visited, int parent) {
+    visited[v] = true;
+    for (int u : tree[v]) {
+        if (!visited[u]) {
+            if (DFS(u, tree, visited, v)) {
+                return true;
+            }
+        }
+        else if (u != parent) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool NotLoop(Graph& g, int u, int v) {
+    vector<vector<int>> tree(g.vertices);
+    for (const Edge& e : g.T) {
+        tree[e.u].push_back(e.v);
+        tree[e.v].push_back(e.u);
+    }
+    vector<bool> visited(g.vertices, false);
+    return DFS(u, tree, visited, -1);
+}
+
+void Kruskal(Graph g, int& nT, vector<Edge>& T) {
+    int nEdge;
+    vector<Edge> listE;
+
+    Init_Arr_Edge(nEdge, listE, g);
+    Sort_Edge(listE, nEdge); // S?a truy?n vào s? c?nh
+
+    nT = 0;
+    int iMin = 0;
+
+    while (nT < g.vertices - 1 && iMin < nEdge) {
+        int u = listE[iMin].u;
+        int v = listE[iMin].v;
+
+        if (!NotLoop(g, u, v)) {
+            T.push_back(listE[iMin]);
+            nT++;
+        }
+        iMin++;
+    }
+}
+
+void edgeTree(vector<Edge>& e, ostream& os) {
+    for (size_t i = 0; i < e.size(); i++) {
+        os << "(" << e[i].u << "," << e[i].v << ") : " << e[i].w;
+        if (i != e.size() - 1) os << "; ";
+    }
+    os << "\n";
+}
+
+int SumT(vector<Edge>& e) {
+    int totalW = 0;
+    for (int i = 0; i < e.size(); i++) {
+        totalW += e[i].w;
+    }
+    return totalW;
+}
+
+void printSpanningTree(Graph& g, string fn) {
+    ofstream f(fn);
+    if (f.is_open()) {
+        int nT = 0;
+        Kruskal(g, nT, g.T);
+
+        f << "Do thi co ma tran la:\n";
+        for (int i = 0; i < g.vertices; i++) { 
+            for (int j = 0; j < g.vertices; j++) {
+                f << setw(4) << g.saveMatrix[i][j];
+            }
+            f << "\n";
+        }
+        f << "Tong cua trong so cua do thi cay khung la: " << SumT(g.T) << "\n";
+        f << "Cac canh cua cay khung:\n";
+        edgeTree(g.T, f);
+        f.close();
+    }
+    else {
+        cout << "Khong mo duoc file output!!" << endl;
+    }
+}
+
+int main() {
+    Graph g;
+    readGraph(g, "input.txt");
+    printSpanningTree(g, "output.txt");
+    return 0;
+}
+
+*/
