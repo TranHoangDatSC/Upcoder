28bd1c304380ee2eedc896d93b4778281151dc2e
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-11-05 15:26:29 +0700

    Update TKTuanTu5.cpp

--- Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTuanTu5.cpp
+++ Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTuanTu5.cpp
@@ -2,40 +2,49 @@
 #include <cmath>
 using namespace std;
 
-int linear_search(int arr[],int target,int n,int max_diff)
-{
-    int index = -1;
-    int min_diff = max_diff + 1;
-    for(int i = 0; i < n; i++)
-    {
-        int diff = abs(arr[i] - target);
-        
-        if(diff <= max_diff && diff < min_diff)
-        {
-            min_diff = diff;
-            index = i;
-        }
-        
-        if(arr[i] == target)
-        {
-            index = i;
-            break;
-        }
+const int MAX = 100;
+
+int linear_search(int arr[],int size,int key) {
+    int max = -10e6;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] > max && arr[i] <= key)
+            max = arr[i];
     }
-    return index;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == max && max != -10e6)
+            return i;
+    }
+    return -1;
 }
 
-int main()
-{
-    int n, target, diff;
-    cin >> n >> target >> diff;
-    int arr[n];
-    
-    for(auto &x : arr)
-        cin >> x;
-        
-    int res = linear_search(arr,target,n,diff);
-    cout << res;
-    
+int array[MAX];
+int size = 0;
+int key;
+
+void input() {
+    cin >> key;
+    while(cin >> array[size])
+        size++;
+}
+
+void output() {
+    if(linear_search(array,size,key) != -1) {
+        int max = -10e6;
+        for(int i = 0; i < size; i++) {
+            if(array[i] > max && array[i] <= key) {
+                max = array[i];
+            }
+        }
+        for(int i = 0; i < size; i++) {
+            if(array[i] == max && max != -10e6)
+                cout << i << " ";
+        }
+    }
+    else cout << -1;
+}
+
+int main() {
+    input();
+    output();
     return 0;
 }
