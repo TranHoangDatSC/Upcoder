5d940a1e1d587b71f074aeff3a14b9ae6c5b5e68
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-11-05 14:57:59 +0700

    Update TKTuanTu3.cpp

--- Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTuanTu3.cpp
+++ Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTuanTu3.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int linear_search(int arr[], int cnt, int x)
-{
-    int res = -10e6;
-    
-    for(int i = 0; i < cnt; i++)
-    {
-        if(arr[i] <= x && arr[i] > res)
-        {
-            res = arr[i];
-        }
+const int MAX = 100;
+
+int linear_search(int arr[],int size,int key) {
+    int max = -10e6;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] > max && arr[i] <= key)
+            max = arr[i];
     }
-    for(int i = 0; i < cnt; i++)
-    {
-        if(res == arr[i] && res != -10e6)
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == max && max != -10e6)
             return i;
     }
     return -1;
 }
 
-int main()
-{
-    int x;
-    cin >> x;
-    
-    int cnt(0);
-    int arr[100];
-    
-    while(cin >> arr[cnt])
-    {
-        cnt++;
-    }
-    
-    cout << linear_search(arr,cnt,x);
+int array[MAX];
+int size = 0;
+int key;
+
+void input() {
+    cin >> key;
+    while(cin >> array[size])
+        size++;
+}
+
+void output() {
+    cout << linear_search(array,size,key);
+}
+
+int main() {
+    input();
+    output();
     return 0;
 }
