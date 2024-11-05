b683113a29805d0bc67f77864badf350c3ce72bf
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-11-05 15:26:51 +0700

    Update TKTuanTu4.cpp

--- Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTuanTu4.cpp
+++ Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKTuanTu4.cpp
@@ -1,54 +1,50 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int linear_search(int arr[], int n, int x)
-{
-    int res = -10e6;
-    
-    for(int i = 0; i < n; i++)
-    {
-        if(arr[i] <= x && arr[i] > res)
-            res = arr[i];
+const int MAX = 100;
+
+int linear_search(int arr[],int size,int key) {
+    int max = -10e6;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] > max && arr[i] <= key)
+            max = arr[i];
     }
-    
-    for(int i = 0; i < n; i++)
-    {
-        if(arr[i] == res && arr[i] != -10e6)
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == max && max != -10e6)
             return i;
     }
     return -1;
 }
 
-int main()
-{
-    int x;
-    cin >> x;
-    
-    int n = 0;
-    int arr[100];
-    
-    while(cin >> arr[n])
-    {
-        n++;
-    }
-    
-    if(linear_search(arr,n,x) != -1)
-    {
-        int res = -10e6;
-        for(int i = 0; i < n; i++)
-        {
-            if(arr[i] <= x && arr[i] > res)
-            {
-                res = arr[i];
+int array[MAX];
+int size = 0;
+int key;
+
+void input() {
+    cin >> key;
+    while(cin >> array[size])
+        size++;
+}
+
+void output() {
+    if(linear_search(array,size,key) != -1) {
+        int max = -10e6;
+        for(int i = 0; i < size; i++) {
+            if(array[i] > max && array[i] <= key) {
+                max = array[i];
             }
         }
-        for(int i = 0; i < n; i++)
-        {
-            if(arr[i] != -10e6 && arr[i] == res)
+        for(int i = 0; i < size; i++) {
+            if(array[i] == max && max != -10e6)
                 cout << i << " ";
         }
     }
-    
     else cout << -1;
+}
+
+int main() {
+    input();
+    output();
     return 0;
 }
