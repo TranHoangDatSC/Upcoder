b8ed897bc7a735542a9b11b084828aa2dfb2253c
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-11-05 14:16:46 +0700

    Update TKNhiPhan.cpp

--- Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKNhiPhan.cpp
+++ Study-Code/Upcoder/CTDL - Data_Structure / Tim_Kiem - Search/TKNhiPhan.cpp
@@ -1,96 +1,49 @@
 #include <iostream>
-#include <vector> 
+#include <cmath>
 using namespace std;
 
-int binary_search(int arr[], int n, int x)
-{
-    int lef = 0, rig = n - 1;
+const int MAX = 100;
+
+int binary_search(int arr[],int size,int key) {
+    int right = size - 1, left = 0;
     
-    while(lef <= rig)
-    {
-        int m = (rig + lef) / 2;
+    while(left <= right) {
+        int mid = (left + right)/2;
         
-        if(arr[m] == x)
-        {
-            return m; // complete to search
+        if(arr[mid] == key) {
+            return mid;
         }
-        else if(arr[m] < x)
-        {
-            lef = m + 1; // find on right
+        else if(arr[mid] < key) {
+            left = mid + 1;
         }
-        else
-        {
-            rig = m - 1; // find on left
+        else if(arr[mid] > key) {
+            right = mid - 1;
         }
     }
-    return -1; // lef > rig
+    return -1;
 }
 
-int main()
-{
-    int n, x;
-    cin >> n >> x;
-    
-    int arr[n];
-    int cnt = 0;
-    
-    for(int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
+int array[MAX];
+int size, key;
+
+void input() {
+    cin >> size >> key;
+    for(int i = 0; i < size; i++) {
+        cin >> array[i];
     }
-    
-    int res = binary_search(arr,n,x);
-    cout << res;
-    
+}
+
+void output() {
+    if(binary_search(array,size,key) != -1) {
+        for(int i = 0; i < size; i++)
+            if(array[i] == key) 
+                cout << i << " ";
+    }
+    else cout << -1;
+}
+
+int main() {
+    input();
+    output();
     return 0;
 }
-/*
-#include <iostream>
-using namespace std;
-
-int binary_search_recursive(int arr[], int lef, int rig, int x)
-{
-    if (lef <= rig)
-    {
-        int m = lef + (rig - lef) / 2; // Avoid potential overflow
-        
-        // Check if x is present at mid
-        if (arr[m] == x)
-        {
-            return m;
-        }
-        // If x is greater, search in the right half
-        else if (arr[m] < x)
-        {
-            return binary_search_recursive(arr, m + 1, rig, x);
-        }
-        // If x is smaller, search in the left half
-        else
-        {
-            return binary_search_recursive(arr, lef, m - 1, x);
-        }
-    }
-    
-    return -1; // Element not found
-}
-
-int main()
-{
-    int n, x;
-    cin >> n >> x;
-    
-    int arr[n];
-    
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    
-    // Call the recursive binary search function
-    int res = binary_search_recursive(arr, 0, n - 1, x);
-    cout << res;
-    
-    return 0;
-}
-
-*/
