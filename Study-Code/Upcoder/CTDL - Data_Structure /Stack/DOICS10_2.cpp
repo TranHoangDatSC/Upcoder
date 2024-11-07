109c8f6520c0a108319254a9ac421d12f6cc49c9
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-11-07 22:02:07 +0700

    Update DOICS10_2.cpp

--- Study-Code/Upcoder/CTDL - Data_Structure /Stack/DOICS10_2.cpp
+++ Study-Code/Upcoder/CTDL - Data_Structure /Stack/DOICS10_2.cpp
@@ -1,30 +1,91 @@
 #include <iostream>
-#include <stack>
-//#include <cstdlib>
 using namespace std;
 
-int main()
-{
-    stack<long> st;
+const int MAX_SIZE = 100;
+
+template<class DataType>
+class Stack {
+    private:
+        DataType *arr;
+        int iTop;
+        int iCapacity;
+    public:
+        Stack();
+        Stack(int maxSize);
+        
+        bool IsEmpty();
+        bool IsFull();
+        
+        void Push(DataType data);
+        DataType Pop();
+        DataType Peek();
+        int GetSize();
+};
+
+template<class DataType>
+Stack<DataType>::Stack() {
+    iCapacity = MAX_SIZE;
+    arr = new DataType [iCapacity];
+    iTop = -1;
+}
+template<class DataType>
+Stack<DataType>::Stack(int maxSize) {
+    iCapacity = maxSize > 0 ? maxSize : MAX_SIZE;
+    arr = new DataType [iCapacity];
+    iTop = -1;
+}
+template<class DataType>
+bool Stack<DataType>::IsEmpty() {
+    return iTop == -1;
+}
+template<class DataType>
+bool Stack<DataType>::IsFull() {
+    return iTop == iCapacity - 1;
+}
+template<class DataType>
+void Stack<DataType>::Push(DataType data) {
+    if(IsFull()) {
+        cout << "FULL";
+    }
+    else {
+        arr[++iTop] = data;
+    }
+}
+template<class DataType>
+DataType Stack<DataType>::Pop() {
+    if(IsEmpty()) {
+        cout << "Empty";
+    }
+    DataType tmp = arr[iTop];
+    iTop--;
+    return tmp;
+}
+template<class DataType>
+DataType Stack<DataType>::Peek() {
+    if(IsEmpty()) cout << "Empty";
+    return arr[iTop];
+    
+}
+
+
+int main() {
+    Stack<long> st;
     long n;
     cin >> n;
     
-    int kq;
-    
-    if(n == 0)
-    {
+    if(n == 0) {
         cout << "0";
-        return 0;    // exit(0);
+        return 0;
     }
-    while(n > 0)
-    {
-        st.push(n % 2);
+    while(n > 0) {
+        st.Push(n % 2);
         n /= 2;
     }
-    while(!st.empty())
-    {
-        cout << st.top();
-        st.pop();
+    
+    while(!st.IsEmpty()) {
+        cout << st.Peek();
+        st.Pop();
     }
+    
     return 0;
 }
