8e0ebf1eca729a708ea431a1d9c9fc33b898544a
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2024-11-07 20:34:54 +0700

    Create STACK1.cpp

---
+++ Study-Code/Upcoder/CTDL - Data_Structure /Stack/STACK1.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+template<class DataType>
+class Stack {
+    private:
+        DataType *arr;
+        int iTop;
+        int iCapacity;
+    public:
+        Stack();
+        Stack(int maxSize);
+        
+        bool IsEmpty();
+        bool IsFull();
+        
+        void Push(DataType data);
+        DataType Pop();
+        DataType Peek();
+        int GetSize();
+};
+
+template<class DataType>
+Stack<DataType>::Stack() {
+    iCapacity = MAX_SIZE;
+    arr = new DataType [iCapacity];
+    iTop = -1;
+}
+template<class DataType>
+Stack<DataType>::Stack(int maxSize) {
+    iCapacity = maxSize > 0 ? maxSize : MAX_SIZE;
+    arr = new DataType [iCapacity];
+    iTop = -1;
+}
+template<class DataType>
+bool Stack<DataType>::IsEmpty() {
+    return iTop == -1;
+}
+template<class DataType>
+bool Stack<DataType>::IsFull() {
+    return iTop == iCapacity - 1;
+}
+template<class DataType>
+void Stack<DataType>::Push(DataType data) {
+    if(IsFull()) {
+        cout << "FULL";
+    }
+    else {
+        arr[++iTop] = data;
+    }
+}
+template<class DataType>
+DataType Stack<DataType>::Pop() {
+    if(IsEmpty()) {
+        cout << "Empty";
+    }
+    DataType tmp = arr[iTop];
+    iTop--;
+    return tmp;
+}
+template<class DataType>
+DataType Stack<DataType>::Peek() {
+    if(IsEmpty()) cout << "Empty";
+    return arr[iTop];
+    
+}
+
+int main() {
+    Stack<char> st;
+    char c;
+    
+    while(cin >> c) {
+        if(c == '*') {
+            if(st.IsEmpty())
+                continue;
+            else {
+                cout << st.Peek();
+                st.Pop();
+            }
+        }
+        else {
+            st.Push(c);
+        }
+    }
+    
+
+    return 0;
+}
